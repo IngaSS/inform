37b7fb00877bb8fba225be6f928b2bb5f77951b2
Inga <guingg@inbox.ru>
2019-09-17 17:27:26 +0300

    17.09.2019

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
       </div>
       <div class="anon animated fadeInDownBig"></div>
       <div class="yellowBlock animated fadeInUpBig"></div>
-      <div class="arrow animated fadeInUpBig" ></div>
+      <div class="arrow animated fadeInUpBig"></div>
     </section>
     <!-- </v-container> -->
   </v-app>
@@ -80,7 +80,7 @@ export default {
   min-height: 10vw;
   background-size: 68%;
   width: 24vw;
-   animation-delay: 1s;
+  animation-delay: 1s;
 }
 
 .arrow {
@@ -91,5 +91,44 @@ export default {
   position: absolute;
   top: 20vw;
   right: 3vw;
+}
+
+@media (max-width: 1024px) {
+  .firstSection {
+    min-height: 75vw;
+    width: 100vw;
+  }
+
+  .introduction {
+    height: 51vw;
+    max-width: 25vw;
+  }
+
+  .introduction h1 {
+    font-size: 3vw;
+    max-width: 25vw;
+  }
+
+  .logo {
+    min-height: 12vw;
+    background-size: 78%;
+    width: 26vw;
+  }
+
+  .anon {
+    width: 60vw;
+    height: 75vw;
+    left: 22.5vw;
+  }
+
+  .yellowBlock {
+    width: 60.7vw;
+  }
+
+  .arrow {
+    min-height: 11vw;
+    width: 8vw;
+    top: 32vw;
+  }
 }
 </style>
